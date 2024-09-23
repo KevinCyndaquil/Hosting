22583aca2739bd3cec00a80cb412115f1b0df104
KevinCyndaquil <aleplantsvsz@gmail.com>
2024-09-22 19:49:24 -0600

    practica 4

--- practica4/includes/sqlclient.php
+++ practica4/includes/sqlclient.php
@@ -1,4 +1,8 @@
 <?php
-    $conn = new mysqli("localhost", "kevincyndaquil", "qw6xdg7sB!", "progwebmov");
+    $host = $_ENV['DB_HOST'];
+    $user = $_ENV['DB_USER'];
+    $passwd = $_ENV['DB_PSWD'];
+
+    $conn = new mysqli($host, $user, $passwd, $passwd);
     if ($conn->connect_error) 
         die("Conection Error: $conn->connect_error");
