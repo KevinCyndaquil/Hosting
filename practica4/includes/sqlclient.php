f078046b42ef5b8c7f69d8c06767a1e69464dacb
KevinCyndaquil <aleplantsvsz@gmail.com>
2024-09-22 21:04:39 -0600

    practica 4

--- practica4/includes/sqlclient.php
+++ practica4/includes/sqlclient.php
@@ -1,8 +1,11 @@
 <?php
-    $host = $_ENV['DB_HOST'];
-    $user = $_ENV['DB_USER'];
-    $passwd = $_ENV['DB_PSWD'];
+    $host = 'sql105.infinityfree.com';
+    $user = 'if0_37364684';
+    $passwd = 'W1hELAmlGZgZJv5';
+    $dbname = 'if0_37364684_progwebmov';
 
-    $conn = new mysqli($host, $user, $passwd, $passwd);
+    $xd = "W1hELAmlGZgZJv5";
+
+    $conn = new mysqli($host, $user, $passwd, $dbname);
     if ($conn->connect_error) 
         die("Conection Error: $conn->connect_error");
